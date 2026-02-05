bab1007b56230e7160bdf24a89a1aa8f0a76673a
msangi2002-byte <msangi2002@gmail.com>
2026-02-06 02:27:27 +0300

    pop

---
+++ src/main/java/com/social/wakify/WakifyApplication.java
@@ -0,0 +1,13 @@
+package com.social.wakify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/wakify/WakifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.wakify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
